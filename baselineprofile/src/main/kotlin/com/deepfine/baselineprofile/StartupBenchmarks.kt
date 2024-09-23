1b656e8b3cfb696fba52dacccf473cb7348cee0d
Yechan Park <yc.park@deepfine.com>
2024-09-23 15:50:46 +0900

    launch spotless

--- baselineprofile/src/main/kotlin/com/deepfine/baselineprofile/StartupBenchmarks.kt
+++ baselineprofile/src/main/kotlin/com/deepfine/baselineprofile/StartupBenchmarks.kt
@@ -69,7 +69,7 @@ class StartupBenchmarks {
         // Check the UiAutomator documentation for more information on how to
         // interact with the app.
         // https://d.android.com/training/testing/other-components/ui-automator
-      }
+      },
     )
   }
 }
